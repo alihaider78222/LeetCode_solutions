95f3393a6e84f825b8d133ae10e8ee9992875075
Ali Haider <ah@embrace-it.com>
2023-01-02 20:54:52 +0500

    Create Solution - 8. String to Integet Atoi

---
+++ dart/8_string_to_integer_atoi.dart
@@ -0,0 +1,153 @@
+/*
+
+  Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+  The algorithm for myAtoi(string s) is as follows:
+
+  Read in and ignore any leading whitespace.
+  Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+  Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+  Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+  If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+  Return the integer as the final result.
+  Note:
+
+  Only the space character ' ' is considered a whitespace character.
+  Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+  
+
+  Example 1:
+
+  Input: s = "42"
+  Output: 42
+  Explanation: The underlined characters are what is read in, the caret is the current reader position.
+  Step 1: "42" (no characters read because there is no leading whitespace)
+          ^
+  Step 2: "42" (no characters read because there is neither a '-' nor '+')
+          ^
+  Step 3: "42" ("42" is read in)
+            ^
+  The parsed integer is 42.
+  Since 42 is in the range [-231, 231 - 1], the final result is 42.
+  Example 2:
+
+  Input: s = "   -42"
+  Output: -42
+  Explanation:
+  Step 1: "   -42" (leading whitespace is read and ignored)
+              ^
+  Step 2: "   -42" ('-' is read, so the result should be negative)
+              ^
+  Step 3: "   -42" ("42" is read in)
+                ^
+  The parsed integer is -42.
+  Since -42 is in the range [-231, 231 - 1], the final result is -42.
+  Example 3:
+
+  Input: s = "4193 with words"
+  Output: 4193
+  Explanation:
+  Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+          ^
+  Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+          ^
+  Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+              ^
+  The parsed integer is 4193.
+  Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+  
+
+  Constraints:
+
+  0 <= s.length <= 200
+  s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+
+*/
+
+import 'dart:math';
+
+/// @author : Ali Haider @alihaider78222
+
+void main() {
+  // String s = "     6878123";     /// => expected result = 6878123
+  // String s = "words and 987";    /// => expected result = 0
+  // String s = "   -42";           /// => expected result = -42
+  // String s = "-91283472332";     /// => expected result = -2147483648
+  // String s = "3.14159";          /// => expected result = 3
+  // String s = " ";                /// => expected result = 0
+  // String s = "  -0012a42";       /// => expected result = -12
+  // String s = "   +0 123";        /// => expected result = 0
+  // String s = "-5-";              /// => expected result = -5
+  String s = "123-"; // => expected result = 123
+
+  var resp = myAtoi(s);
+  print(resp.toString());
+}
+
+int myAtoi(String s) {
+  if (s.length == 0 || s.length > 200) {
+    return 0;
+  }
+
+  s = s.trim();
+
+  List<String> characters = s.split('');
+  var result = '';
+
+  // characters.removeWhere((element) => element == ' ');
+
+  if (characters.isEmpty) {
+    return 0;
+  }
+
+  bool characterOrSpaceFound = false;
+  int i = 0;
+
+  while (i < characters.length && characterOrSpaceFound == false) {
+    if (isDigit(characters[i]) ||
+        characters[i] == '-' ||
+        characters[i] == '+' ||
+        characters[i] == '.') {
+      // check if numeric sign is already in the string then discard next characters
+      if ((result.contains('-') ||
+              result.contains('+') ||
+              result.contains('.')) &&
+          (characters[i] == '-' ||
+              characters[i] == '+' ||
+              characters[i] == '.')) {
+        characterOrSpaceFound = true;
+      } else {
+        result += characters[i];
+      }
+    } else {
+      characterOrSpaceFound = true;
+    }
+    i++;
+  }
+
+  // Edge case if numeric expression at the end of the value
+  if (result.endsWith('-') || result.endsWith('+') || result.endsWith('/')) {
+    result = result.substring(0, result.length - 1);
+  }
+
+  int number = (double.tryParse(result) ?? 0).toInt();
+
+  num maxConstraints = pow(2, 31) - 1;
+  num minConstraints = pow(-2, 31);
+
+  if (number < minConstraints) {
+    number = -maxConstraints - 1 as int;
+  }
+  if (number > maxConstraints) {
+    number = maxConstraints as int;
+  }
+
+  return number;
+}
+
+bool isDigit(String s) {
+  if (s == null || s == '') {
+    return false;
+  }
+  return int.tryParse(s) != null;
+}
